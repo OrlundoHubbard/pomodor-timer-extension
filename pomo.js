39b8d764f370ab6a00aacf7d4ae9fb3a41462262
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-03-30 21:43:27 -0500

    first commit

---
+++ pomo.js
@@ -0,0 +1,7 @@
+// Define the length of a pomodoro and a short break
+const pomodoroLength = 25 * 60;
+const shortBreakLength = 5 * 60;
+
+// Initialize the timer with the pomodoro length
+let timerLength = pomodoroLength;
+let timerId;
