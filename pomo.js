4ff137b72c0e1c435bb491c0100e2da1ad851470
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-04-01 23:20:28 -0500

    more js

--- pomo.js
+++ pomo.js
@@ -5,3 +5,14 @@ const shortBreakLength = 5 * 60;
 // Initialize the timer with the pomodoro length
 let timerLength = pomodoroLength;
 let timerId;
+
+// Get references to the UI elements
+const timerDisplay = document.getElementById('timer-display');
+const startButton = document.getElementById('start-button');
+const pauseButton = document.getElementById('pause-button');
+const resetButton = document.getElementById('reset-button');
+
+// Define functions to update the timer display and and handle button clicks
+function updateTimerDisplay(timeLeft) {
+    const minutes = Math.floor(timeLeft / 60);
+}
